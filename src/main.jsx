7e03b1adfad179389c6deaf956b5d49c3e1ab2e1
Harshiile~ <theharshiile@gmail.com~>
2025-01-24 17:44:08 +0530

    Template ready

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css'
+import App from './App.jsx'
+import Login from './pages/Login.jsx';
+
+createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App />} />
+			<Route path='/login' element={<Login />} />
+		</Routes>
+	</BrowserRouter>
+)
